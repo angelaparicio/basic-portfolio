f8cd9abefa54fdde534adebd9da895c4f612adc8
Angel <a.l.aparicio.gomez@gmail.com>
2021-12-18 17:36:22 +0100

    Add years to shortcode

--- shortcode.php
+++ shortcode.php
@@ -11,13 +11,30 @@
 		
 		foreach ( $posts as $post ):
 			
+			$work_start_year = get_post_meta( $post->ID, 'work_start_year', true );
+			$work_end_year = get_post_meta( $post->ID, 'work_end_year', true );
+			
+			$years = '';
+			if ( $work_start_year && $work_end_year ){
+				$years = ' ('.$work_start_year.' - '.$work_end_year.')';
+			}
+			else 
+			if ( $work_start_year && !$work_end_year ){
+				$years = ' ('.$work_start_year.')';
+			}
+			else 
+			if ( !$work_start_year && $work_end_year ){
+				$years = ' ('.$work_end_year.')';
+			}
+			
+			
 			$shortcode .= '<div id="work-porfolio-element-'.$post->ID.'" class="work-porfolio-element">';
 				
 				$shortcode .= '<a href="'.$post->guid.'">';
 					$shortcode .= '<div class="work-porfolio-image">';
 						$shortcode .= get_the_post_thumbnail($post->ID);
 					$shortcode .= '</div>';
-					$shortcode .= '<div class="work-porfolio-title">'.$post->post_title.'</div>';
+					$shortcode .= '<div class="work-porfolio-title">'.$post->post_title.$years.'</div>';
 				$shortcode .= '</a>';
 				 
 			$shortcode .= '</div>';
